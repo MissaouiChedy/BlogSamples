a027aeb5035d279dffbf8d8eee22c7b571dd8b07
Chedy Missaoui <missaoui.chedy@gmail.com>
2024-12-08 11:00:28 +0100

    Azure Event hubs sending example comments

--- AzureEventHubSendingMessages/AzureEventHubSendingMessages/Program.cs
+++ AzureEventHubSendingMessages/AzureEventHubSendingMessages/Program.cs
@@ -5,32 +5,47 @@ using System.Text;
 using System.Text.Json;
 
 int eventsCount = 10;
-
+/*
+ * Initialize the Producer Client Object
+ */
 EventHubProducerClient evhProducer = new EventHubProducerClient(
     "evh-test-sending.servicebus.windows.net",
     "main-topic",
     new DefaultAzureCredential());
 
+/*
+ * Use the Producer Client to create an event batch
+ */
 using EventDataBatch eventBatch = await evhProducer.CreateBatchAsync();
 
-Message message = new(
-    Guid.NewGuid(),
-    DateTime.UtcNow,
-    "KABLAM"
-);
-
-// Create a Batch of Messages
+/*
+ * Create a Batch of Messages
+ */
 Enumerable
     .Range(0, eventsCount)
     .ToList()
     .ForEach((_) =>
     {
-        if (!eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message)))))
+        Message message = new(
+            Guid.NewGuid(),
+            DateTime.UtcNow,
+            "KABLAM"
+        );
+
+        byte[] messageInBinary = Encoding
+            .UTF8
+            .GetBytes(JsonSerializer.Serialize(message));
+
+        if (!eventBatch.TryAdd(new EventData(messageInBinary)))
         {
             throw new Exception($"Event is too large for the batch and cannot be sent.");
         }
     });
 
+/*
+ * Send The Event Batch, read key from console,
+ * then dispose of Producer Client
+ */
 try
 {
     await evhProducer.SendAsync(eventBatch);
@@ -42,4 +57,7 @@ finally
     await evhProducer.DisposeAsync();
 }
 
+/*
+ * Message Record Definition
+ */
 readonly record struct Message(Guid Id, DateTime TimeStamp, string Content) { }
