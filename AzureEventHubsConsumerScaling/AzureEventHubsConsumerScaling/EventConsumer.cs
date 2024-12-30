39ffc06cdde871905481bd11352d0eafde45eb0a
Chedy Missaoui <missaoui.chedy@gmail.com>
2024-12-30 13:33:51 +0100

    WIP: Consumer Scaling

---
+++ AzureEventHubsConsumerScaling/AzureEventHubsConsumerScaling/EventConsumer.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Processor;
+using System.Text.Json;
+
+namespace AzureEventHubsConsumerScaling
+{
+    public class EventConsumer
+    {
+        private readonly EventProcessorClient _eventProcessorClient;
+        private readonly int _id;
+        private readonly List<MessageReceived> _receivedMessages = new List<MessageReceived>();
+        
+        public int Id { get => _id; }
+        
+        public EventConsumer(int id, EventProcessorClient eventProcessorClient)
+        {
+            _id = id;
+            _eventProcessorClient = eventProcessorClient;
+
+            _eventProcessorClient.ProcessEventAsync += ProcessEventHandler;
+            _eventProcessorClient.ProcessErrorAsync += ProcessErrorHandler;
+        }
+
+        public Task StartConsumptionAsync()
+        {
+            return _eventProcessorClient.StartProcessingAsync();
+        }
+
+        public async Task<List<MessageReceived>> StopConsumptionAsync()
+        {
+            await _eventProcessorClient.StopProcessingAsync();
+            return _receivedMessages;
+        }
+
+        protected Task ProcessEventHandler(ProcessEventArgs eventArgs)
+        {
+            var readOnlySpan = new ReadOnlySpan<byte>(eventArgs.Data.Body.ToArray());
+            Message receivedMessage = JsonSerializer
+                .Deserialize<Message>(readOnlySpan)!;
+
+            Console.WriteLine($"Consumer {_id} received '{receivedMessage}' from partition {eventArgs.Partition.PartitionId}");
+            _receivedMessages.Add(new(eventArgs.Partition.PartitionId, receivedMessage));
+            return eventArgs.UpdateCheckpointAsync();
+        }
+
+        protected Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+        {
+            Console.WriteLine(@$"\tPartition '{eventArgs.PartitionId}':
+    an unhandled exception was encountered. This was not expected to happen.");
+            Console.WriteLine(eventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+    }
+
+    public record Message(Guid Id, string LocationId, string Content);
+    public record MessageReceived(string PartitionId, Message Message);
+}
--- AzureEventHubsConsumerScaling/AzureEventHubsConsumerScaling/Program.cs
+++ AzureEventHubsConsumerScaling/AzureEventHubsConsumerScaling/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * https://blog.pnop.co.jp/jmeter-azure-event-hubs_en/jj
+ */
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using AzureEventHubsConsumerScaling;
+using System.Collections.Concurrent;
+
+int consumersCount = 3;
+ConcurrentBag<MessageReceived> receivedMessages = new ConcurrentBag<MessageReceived>();
+
+var consumers = Enumerable
+    .Range(0, consumersCount)
+    .Select((i) =>
+    {
+        var storageClient = new BlobContainerClient(
+            new Uri("https://mainconsumerstorageacc.blob.core.windows.net/main-consumer"),
+            new DefaultAzureCredential());
+
+        var processor = new EventProcessorClient(
+            storageClient,
+            "main-consumer",
+            "evh-test-consume-scaling.servicebus.windows.net",
+            "main-topic",
+            new DefaultAzureCredential());
+
+        var eventConsumer = new EventConsumer(i, processor);
+        eventConsumer.StartConsumptionAsync();
+
+        return eventConsumer;
+    })
+    .ToList();
+
+Console.WriteLine($"All '{consumersCount}' Consumers started, waiting for console input to stop...");
+Console.ReadKey();
+
+Console.WriteLine("");
+Console.WriteLine("=================================================");
+foreach (var c in consumers)
+{
+    var messagesReceived = await c.StopConsumptionAsync();
+    Console.WriteLine($"Consumer {c.Id} Received '{messagesReceived.Count}' Messages");
+
+    foreach (var m in messagesReceived)
+    {
+        receivedMessages.Add(m);
+    }
+}
+
+Console.WriteLine($"Received '{receivedMessages.Count}' Messages");
+
+string locationIdsForParition0 = string.Join(",", receivedMessages
+    .Where(m => m.PartitionId == "0")
+    .Select(m => m.Message.LocationId)
+    .OrderBy(id => id)
+    .Distinct());
+
+var locationIdsForParition1 = string.Join(",", receivedMessages
+    .Where(x => x.PartitionId == "1")
+    .Select(x => x.Message.LocationId)
+    .OrderBy(x => x)
+    .Distinct());
+
+Console.WriteLine($"Location Ids received on Partition 0 '{locationIdsForParition0}'");
+Console.WriteLine($"Location Ids received on Partition 1 '{locationIdsForParition1}'");
+Console.WriteLine("===================================================");
